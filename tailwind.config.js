23c5fb9595fb07aedbf8fd6f430748ccdf117ffc
Gustavo Caetano <gustavo.caetano13@gmail.com>
2023-08-28 22:45:57 -0300

    tudo

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+};
